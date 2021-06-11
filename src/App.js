e009bd253dc282aa8fbd6e32169e48a18b07be2a
TamNguyen <tamkid2408@gmail.com>
2021-06-11 16:58:59 +0700

    deploy on surge server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import productApi from 'api/productApi';
 import Header from 'components/Header';
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import NotFound from './components/NotFound';
@@ -7,6 +8,28 @@ import NotFound from './components/NotFound';
 const Photo = React.lazy(() => import('./features/Photo'));
 
 function App() {
+    const [productList, setProductList] = useState([]);
+
+    // Test api
+    useEffect(() => {
+        const fetchProductList = async () => {
+            try {
+                const params = {
+                    _limit: 10,
+                    _page: 1
+                };
+                const response = await productApi.getAll(params);
+                console.log(response);
+
+                setProductList(response.data);
+            } catch (error) {
+                console.log("Fetch Product List Fail", error);
+            }
+        };
+
+        fetchProductList();
+    }, []);
+
     return (
         <div className="photo-app">
             <Suspense fallback={<div>Loading....</div>}>
